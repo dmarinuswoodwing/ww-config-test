50acb0a88fdc18db56cd9e39d529b1a3977f9352
Oleksii Liashuk <yxycman@gmail.com>
2022-03-09 16:26:13 +0200

    adding S998882 assets plugins folder

---
+++ S998882/plugins/trashcan/move_to_trashcan/1.3/settings.js
@@ -0,0 +1,4 @@
+pluginSettings = {
+    "api_key": "mJTuV4T1cQ7D8CJH0LJp7QU82mCYVhpa7imrpwZd",
+    "trashpath": "/Trash",
+}
---
+++ S998882/plugins/trashcan/trashcan/1.3/settings.js
@@ -0,0 +1,7 @@
+pluginSettings = {
+    "api_key": "mJTuV4T1cQ7D8CJH0LJp7QU82mCYVhpa7imrpwZd",
+    "server_url": "https://e912-92-40-201-109.eu.ngrok.io",
+    "trashpath": "/Trash",
+    "username": "admin",
+    "password": "1234567"
+}
